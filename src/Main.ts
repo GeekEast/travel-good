94b035008336755218c73d690eedb9680cefe9c1
GeekEast <sx.ba2017@gmail.com>
2019-10-08 09:42:52 +1030

    update

--- src/Main.ts
+++ src/Main.ts
@@ -18,18 +18,22 @@ const main = async (lat: string, lng: string, radius: number, ratings: number, r
 }
 
 // adelaide
-main("-34.928471", "138.593174", 2000, 4.5, 40, "restaurant", "adelaide_restaurants");
-main("-34.928471", "138.593174", 2000, 4.5, 40, "cafe", "adelaide_cafes");
-main("-34.928471", "138.593174", 2000, 4.5, 40, "bar", "adelaide_bars");
-main("-34.928471", "138.593174", 2000, 4.5, 40, "liquor_store", "adelaide_liquor_store");
-main("-34.928471", "138.593174", 2000, 4.5, 40, "clothing_store", "adelaide_clothing_store");
-main("-34.928471", "138.593174", 2000, 4.5, 40, "shoe_store", "adelaide_shoe_store");
-main("-34.928471", "138.593174", 2000, 4.5, 40, "shopping_mall", "adelaide_shopping_mall");
-main("-34.928471", "138.593174", 2000, 4.5, 40, "lodging", "adelaide_lodging");
+// main("-34.928471", "138.593174", 2000, 4.5, 40, "restaurant", "adelaide_restaurants");
+// main("-34.928471", "138.593174", 2000, 4.5, 40, "cafe", "adelaide_cafes");
+// main("-34.928471", "138.593174", 2000, 4.5, 40, "bar", "adelaide_bars");
+// main("-34.928471", "138.593174", 2000, 4.5, 40, "liquor_store", "adelaide_liquor_store");
+// main("-34.928471", "138.593174", 2000, 4.5, 40, "clothing_store", "adelaide_clothing_store");
+// main("-34.928471", "138.593174", 2000, 4.5, 40, "shoe_store", "adelaide_shoe_store");
+// main("-34.928471", "138.593174", 2000, 4.5, 40, "shopping_mall", "adelaide_shopping_mall");
+// main("-34.928471", "138.593174", 2000, 4.5, 40, "lodging", "adelaide_lodging");
 
 // melbourne
-// main("-37.807373", "144.964814", 2000, 4.5, 40, "restaurant", "mel");
+// main("-37.807373", "144.964814", 2000, 4.5, 40, "restaurant", "mel_restaurants");
 
 // sydney
-// main("-33.864064", "151.208064", 2000, 4.5, 40, "restaurant", "sydney");
+// main("-33.864064", "151.208064", 2000, 4.5, 40, "restaurant", "sydney_restaurants");
+
+// brisbane
+main("-27.468972", "153.025152", 2000, 4.5, 40, "restaurant", "brisbane_restaurants");
+
 
